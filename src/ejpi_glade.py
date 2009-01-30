efa516ca38501075074fd0f96632cf75dbe87579
epage <eopage@byu.net>
2009-01-30 01:23:12 +0000

    Set the version to where I feel it is
    git-svn-id: file:///svnroot/ejpi/trunk@3 df6cc7de-23d0-4ae0-bb86-c17aa67b2a9d

--- src/ejpi_glade.py
+++ src/ejpi_glade.py
@@ -137,7 +137,7 @@ class Calculator(object):
 
 	__pretty_app_name__ = "e^(j pi) + 1 = 0"
 	__app_name__ = "ejpi"
-	__version__ = "0.1.0"
+	__version__ = "0.9.0"
 	__app_magic__ = 0xdeadbeef
 
 	_glade_files = [
