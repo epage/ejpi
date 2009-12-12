101f252e9c764c276c29e0a507c7681bc582d915
epage <eopage@byu.net>
2009-12-12 05:26:17 +0000

    Major version bump
    git-svn-id: file:///svnroot/ejpi/trunk@80 df6cc7de-23d0-4ae0-bb86-c17aa67b2a9d

--- src/constants.py
+++ src/constants.py
@@ -2,8 +2,8 @@ import os
 
 __pretty_app_name__ = "e**(j pi) + 1 = 0"
 __app_name__ = "ejpi"
-__version__ = "0.9.6"
-__build__ = 8
+__version__ = "0.9.7"
+__build__ = 9
 _data_path_ = os.path.join(os.path.expanduser("~"), ".ejpi")
 __app_magic__ = 0xdeadbeef
 _user_logpath_ = "%s/ejpi.log" % _data_path_
